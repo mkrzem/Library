6f5bd92ca6accb0c30ed9d83d87dc77becb5470e
unknown <mkrzeminski@MKRZEMINSKI-RZE.pgs-soft.com>
2015-08-27 09:22:01 +0200

    Started Angular development. Routing added for SPA. Adding Book controller added.

---
+++ ClassicLibrary/Scripts/app/Controllers/AddBookController.js
@@ -0,0 +1,17 @@
+﻿app.controller('AddBookController', ['$scope', '$location',  function ($scope, $location) {
+    $scope.newBook= {
+        Name: '',
+        Genre: 'Drama',
+        Author: '',
+        Status: 'Available',
+        Quantity: 1
+    };
+
+    $scope.$location = $location;
+
+    //$scope.save = function() {
+    //    var result = bookService.save($scope.newBook.Name, $scope.newBook.Genre, $scope.newBook.Author);
+
+        
+   // };
+}]);
--- ClassicLibrary/Scripts/app/app.js
+++ ClassicLibrary/Scripts/app/app.js
@@ -1,7 +1,22 @@
 ﻿var app = angular.module('LibraryApp', ['ngRoute']);
-
-app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.
-        when('/', {
+    
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/Books', {
+            controller: 'HomeController',
+            templateUrl: '/Templates/BookList.html'
         })
-}])
+        .when('/AddBook', {
+            controller: 'AddBookController',
+            templateUrl: '/Templates/Add.html'
+        })
+       // .when('/YourBooks')
+        .otherwise({
+            redirectTo: '/'
+        });
+
+    $locationProvider.html5Mode({
+        enable: true,
+        requireBase: false
+    });
+}]);
